a258bebc62e06fd5a15edb83374528297f20aecd
Julien Demaere <julienljdemaere@gmail.com>
2023-09-22 16:12:11 -0700

    Added GPT 35 turbo to open.ai lib

--- jupyter-notebooks/lib/openai.py
+++ jupyter-notebooks/lib/openai.py
@@ -11,7 +11,19 @@ def gpt4(prompt):
 		model="gpt-4",
     temperature=0.0,
 		messages=[
-      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "system", "content": "You are a material science PHD student."},
+      {"role": "user", "content": f"{prompt}"}
+		]
+	)
+
+	return completion.choices[0].message.content
+	
+def gpt_35_turbo_16k(prompt):
+	completion = openai.ChatCompletion.create(
+		model="gpt-3.5-turbo-16k",
+    temperature=0.0,
+		messages=[
+      {"role": "system", "content": "You are a material science PHD student."},
       {"role": "user", "content": f"{prompt}"}
 		]
 	)
